1c1a2d7316c21e1eb06ea48040c2df0acf558a3f
Mateus Batista <mateusbatistasantos@gmail.com>
2016-08-25 09:12:12 -0300

    Redirencionando output dos scripts para /dev/null. Melhorando a verficação dos projetos para iniciar ambientes automaticamente. Acrescentando alias para abrir trabalho sem iniciar projetos.

--- workspace.sh
+++ workspace.sh
@@ -2,8 +2,8 @@
 #Let's get in formation
 
 #Open Slack
-slack &
+slack > /dev/null 2>&1 &
 #Open Sublime
-subl &
+subl > /dev/null 2>&1 &
 #Open Chrome
-google-chrome --app="http://www.pivotaltracker.com" &
+google-chrome --app="http://www.pivotaltracker.com" > /dev/null 2>&1 &
