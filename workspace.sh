7f762466e65f5ddd0020e75f4daa128c4e5236f1
Mateus Batista <mateusbatistasantos@gmail.com>
2018-01-26 15:50:43 -0200

    Organizing aliases by companing. Removing .env file and hardcoding the source path of mydevops folder.

--- workspace.sh
+++ workspace.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 #Let's get in formation
 
-#Open Slack
-slack > /dev/null 2>&1 &
-#Open Sublime
-subl > /dev/null 2>&1 &
-#Open Chrome
-google-chrome --app="http://www.pivotaltracker.com" > /dev/null 2>&1 &
+#Open my Planners
+subl ~/Planners > /dev/null 2>&1 &
+
+#Open Chromew
+google-chrome > /dev/null 2>&1 &
